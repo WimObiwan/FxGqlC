efc57e706dcf086c24b74a5a053c7fcb18717031
Wim Devos <wim@obiwan.be>
2012-06-18 11:25:18 +0200

    Refactored GqlQueryCommand to use same logic as IntoProvider to format output.

--- FxGql/FxGqlLib/Commands/GqlQueryCommand.cs
+++ FxGql/FxGqlLib/Commands/GqlQueryCommand.cs
@@ -17,33 +17,10 @@ namespace FxGqlLib
 		#region IGqlCommand implementation
 		public void Execute (TextWriter outputStream, TextWriter logStream, GqlEngineState gqlEngineState)
 		{
-//			GqlQueryState gqlQueryState = new GqlQueryState (gqlEngineState.ExecutionState);
-//			gqlQueryState.CurrentDirectory = gqlEngineState.CurrentDirectory;
-//
-//			IntoProvider.DumpProviderToStream (gqlQuery, outputStream, gqlQueryState, "\t", Environment.NewLine);
+			GqlQueryState gqlQueryState = new GqlQueryState (gqlEngineState.ExecutionState);
+			gqlQueryState.CurrentDirectory = gqlEngineState.CurrentDirectory;
 
-			FormatColumnListFunction formatColumnListFunction = new  FormatColumnListFunction ("\t");
-			using (SelectProvider provider = new SelectProvider (
-					new List<IExpression> () { formatColumnListFunction },
-					gqlQuery)) {
-				
-				try {
-					GqlQueryState gqlQueryState = new GqlQueryState (gqlEngineState.ExecutionState);
-					gqlQueryState.CurrentDirectory = gqlEngineState.CurrentDirectory;
-					provider.Initialize (gqlQueryState);
-
-
-					while (provider.GetNextRecord()) {
-						string text = provider.Record.Columns [0].ToString ();
-						outputStream.WriteLine (text);
-						if (logStream != null)
-							logStream.WriteLine (text);
-					}
-					
-				} finally {
-					provider.Uninitialize ();
-				}
-			}
+			IntoProvider.DumpProviderToStream (gqlQuery, outputStream, gqlQueryState, "\t");
 		}
 		#endregion
 	}
