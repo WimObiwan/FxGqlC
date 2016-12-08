f4e1f999b7956fed9abe2176b78f5670d1092ea3
Wim Devos <wim@obiwan.be>
2016-12-08 13:32:00 +0100

    Chocolatey: Updated build to automatically populate version info in nuspec.

--- Chocolatey/fxgqlc/build.ps1
+++ Chocolatey/fxgqlc/build.ps1
@@ -1,2 +1,37 @@
+$ErrorActionPreference = 'STOP'
+
+# Tools
+git clean -xfd .\Tools\
 cp ..\..\FxGql\FxGqlC\bin\Release\* .\Tools
-choco pack
+$fxgqlc = $(.\Tools\FxGqlC.exe)
+$title = $fxgqlc | ?{ $_ -match '^.*\s(v\d+\.\d+\.\w+)\s.*$' } | select -First 1
+$copyright = $fxgqlc | ?{ $_ -match 'copyright' } | select -First 1
+$version = $title -replace '^.*\s(v\d+\.\d+\.\w+)\s.*$', '$1'
+
+if ($version -match '^v\d+\.\d+\.\d+$') {
+    # release
+    Write-Error 'TO BE TESTED FIRST!'
+    $nuget_version = $version
+} elseif ($version -match '^v(\d+\.\d+)\.(\w+)$') {
+    # pre-release
+    $nuget_version = $Matches[1] + '.0-' + $Matches[2]
+} else {
+    # unknown format
+    Write-Error "Version information invalid ($version)"
+}
+
+Write-Warning "Using version for nuget: $nuget_version"
+
+$fxgqlc_orig = $(gc .\fxgqlc-orig.nuspec)
+$fxgqlc_orig `
+    -replace '<version>.*</version>', "<version>$([Security.SecurityElement]::Escape($nuget_version))</version>" `
+    -replace '<copyright>.*</copyright>', "<copyright>$([Security.SecurityElement]::Escape($copyright))</copyright>" `
+    | sc .\fxgqlc.nuspec
+
+gc .\fxgqlc.nuspec
+
+# Create package
+choco pack .\fxgqlc.nuspec
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "choco pack FAILED! ($LASTEXITCODE)"
+}
