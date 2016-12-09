9c6fe52cb4c200e235a171ed10221364d8ed751e
Wim Devos <wim@obiwan.be>
2016-12-09 11:22:40 +0100

    FxGqlC: Added version comments.

--- FxGql/FxGqlC/AssemblyInfo.cs
+++ FxGql/FxGqlC/AssemblyInfo.cs
@@ -17,9 +17,19 @@ using System.Runtime.CompilerServices;
 // http://en.wikipedia.org/wiki/Software_release_life_cycle
 // http://en.wikipedia.org/wiki/Software_versioning
 
-// Update both fields!
-[assembly: AssemblyVersion ("2.6.0.10")]
-[assembly: AssemblyInformationalVersion("v2.6.0-alpha10")]
+// Examples:
+//   AssemblyVersion ("x.y.z.q") e.g. 2.6.0.17
+//   AssemblyInformationalVersion ("x.y.z[-suffix]") e.g. 2.6.0-beta3
+// Rules:
+//   x++     Every backward compatibility break, e.g. SELECT statements not working anymore because of a (even small) change.
+//   y++     Added functionality, without breaking backward compatibility
+//   z++     Bugfix, internal refactoring, performance increase
+//   q++     To make version unique, if x/y/z remain unchanged.
+//   -suffix Optional indication of a pre-release.  alpha, beta, rc, ...
+// Always update both versions!
+
+[assembly: AssemblyVersion ("2.6.0.10")]            // e.g. 2.6.0.17
+[assembly: AssemblyInformationalVersion("v2.6.0-alpha10")]  // e.g. 2.6.0-beta3
 
 
 // The following attributes are used to specify the signing key for the assembly,
